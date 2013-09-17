a8a82eaf6e3028f185d6f4417b4abd41a688e1ee
Alexandre Delaunay <adelaunay@teclib.com>
2013-09-17 14:42:06 +0200

    show satisfaction tab only on closed status

--- inc/surveyanswer.class.php
+++ inc/surveyanswer.class.php
@@ -50,7 +50,7 @@ class PluginSatisfactionSurveyAnswer extends CommonDBChild {
       global $LANG;
 
       // can exists for template
-      if ($item->getType() == 'Ticket') {
+      if ($item->getType() == 'Ticket' && $item->fields['status'] == "closed") {
          // a survey is active for current entity of item
          $survey = PluginSatisfactionSurvey::getObjectForEntity($item->fields['entities_id']);
          if ($survey !== false) {
