811d1752ee0916ed6754e4343b50583d9fc4f94e
tsmr <thetsmr@gmail.com>
2022-01-14 09:28:27 +0100

    Update headers & int unsigned

--- inc/reminder.class.php
+++ inc/reminder.class.php
@@ -3,7 +3,7 @@
  * @version $Id: HEADER 15930 2011-10-30 15:47:55Z tsmr $
  -------------------------------------------------------------------------
  satisfaction plugin for GLPI
- Copyright (C) 2009-2016 by the satisfaction Development Team.
+ Copyright (C) 2016-2022 by the satisfaction Development Team.
 
  https://github.com/pluginsglpi/satisfaction
  -------------------------------------------------------------------------
--- install/sql/empty-1.6.0.sql
+++ install/sql/empty-1.6.0.sql
@@ -1,43 +1,43 @@
 # noinspection SqlNoDataSourceInspectionForFile
 
 CREATE TABLE `glpi_plugin_satisfaction_surveys` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `entities_id` int(11) NOT NULL DEFAULT 0,
-  `is_recursive` tinyint(1) NOT NULL default '0',
-  `is_active` tinyint(1) NOT NULL default '0',
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `entities_id` int unsigned NOT NULL DEFAULT 0,
+  `is_recursive` tinyint NOT NULL default '0',
+  `is_active` tinyint NOT NULL default '0',
   `name` varchar(255) collate utf8mb4_unicode_ci default NULL,
   `comment` text collate utf8mb4_unicode_ci default NULL,
   `date_creation` timestamp NULL DEFAULT NULL,
   `date_mod` timestamp NULL DEFAULT NULL,
-  `reminders_days` int(11) NOT NULL default '30',
+  `reminders_days` int unsigned NOT NULL default '30',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
 
 CREATE TABLE `glpi_plugin_satisfaction_surveyquestions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `plugin_satisfaction_surveys_id` int(11) NOT NULL,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `plugin_satisfaction_surveys_id` int unsigned NOT NULL,
   `name` text collate utf8mb4_unicode_ci default NULL,
   `type` varchar(255) collate utf8mb4_unicode_ci default NULL,
   `comment` text collate utf8mb4_unicode_ci default NULL,
-  `number` int(11) NOT NULL DEFAULT 0,
-  `default_value` int(11) NOT NULL DEFAULT 1,
+  `number` int unsigned NOT NULL DEFAULT 0,
+  `default_value` int unsigned NOT NULL DEFAULT 1,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
 
 CREATE TABLE `glpi_plugin_satisfaction_surveyanswers` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
   `answer` text collate utf8mb4_unicode_ci default NULL,
   `comment` text collate utf8mb4_unicode_ci default NULL,
-  `plugin_satisfaction_surveys_id` int(11) NOT NULL,
-  `ticketsatisfactions_id` int(11) NOT NULL,
+  `plugin_satisfaction_surveys_id` int unsigned NOT NULL,
+  `ticketsatisfactions_id` int unsigned NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
 
 DROP TABLE IF EXISTS `glpi_plugin_satisfaction_surveytranslations`;
 CREATE TABLE `glpi_plugin_satisfaction_surveytranslations` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `plugin_satisfaction_surveys_id` int(11) NOT NULL DEFAULT '0',
-  `glpi_plugin_satisfaction_surveyquestions_id` int(11) NOT NULL DEFAULT '0',
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `plugin_satisfaction_surveys_id` int unsigned NOT NULL DEFAULT '0',
+  `glpi_plugin_satisfaction_surveyquestions_id` int unsigned NOT NULL DEFAULT '0',
   `language` varchar(5) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `value` text COLLATE utf8mb4_unicode_ci,
   PRIMARY KEY (`id`),
@@ -48,21 +48,21 @@ CREATE TABLE `glpi_plugin_satisfaction_surveytranslations` (
 
 DROP TABLE IF EXISTS `glpi_plugin_satisfaction_surveyreminders`;
 CREATE TABLE `glpi_plugin_satisfaction_surveyreminders` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `plugin_satisfaction_surveys_id` int(11) NOT NULL,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `plugin_satisfaction_surveys_id` int unsigned NOT NULL,
   `name` text collate utf8mb4_unicode_ci default NULL,
-  `duration_type` int(11) NOT NULL,
-  `duration` int(11) NOT NULL,
-  `is_active` tinyint(1) NOT NULL default '0',
+  `duration_type` int unsigned NOT NULL,
+  `duration` int unsigned NOT NULL,
+  `is_active` tinyint NOT NULL default '0',
   `comment` text collate utf8mb4_unicode_ci default NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
 
 DROP TABLE IF EXISTS `glpi_plugin_satisfaction_reminders`;
 CREATE TABLE `glpi_plugin_satisfaction_reminders` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `type` int(11) NOT NULL DEFAULT '0',
-  `tickets_id` int(11) NOT NULL,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `type` int unsigned NOT NULL DEFAULT '0',
+  `tickets_id` int unsigned NOT NULL,
   `date` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unicity` (`tickets_id`)
